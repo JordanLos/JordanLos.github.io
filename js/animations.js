4630240cb20461bf5c4681669b7002ca2de461d8
jordan <jordan.los@hotmail.com>
2015-06-25 21:45:10 -0600

    CSS rules finalized

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,8 @@
 window.onload = function() {
 var icon = document.getElementsByClassName("landing-anim");
 var logo = document.getElementById("logo")
-TweenLite.from(logo, 1, {opacity:0, paddingBottom:"31px"});
-TweenLite.from(icon, 1.5, {opacity:0, paddingBottom:"31px", delay:1.5});
+TweenLite.from(logo, 1, {opacity:0, top:"150px"});
+TweenLite.from(icon, 1.5, {opacity:0, top:"-15px", delay:1.5});
 }
 function moveToSide(e, side) {
 	TweenLite.to(e, 1, {side:"0px"});
