bd3628a91b5357b7ba7a3921a61f99a29fb238ad
jordan <jordan.los@hotmail.com>
2015-09-04 17:24:34 -0600

    Almost Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
-$( window ).load(function() {
-	$( 'img' ).resizeRhythm(12);
+$( document ).ready(function() {
+
+	$('.clickTab').click(function() {
+		$( this ).addClass( "focus" );
+		$( '.clickTab' ).not( $( this ) ).removeClass( 'focus' );
+		
+		var self  = $( this ).index();
+		var	matchedTarget = $( '.clickTarget' ).eq(self);
+
+		matchedTarget.css("display", "block" );
+
+		if ($( this ).index() <= 2)
+			$( '#park, #beach, #zoo' ).not(matchedTarget).css("display", "none");
+		else 
+			$( '#mon, #tues, #wed' ).not(matchedTarget).css("display", "none");
+		
+
+	//need to refactor the HTML
+
+
+	});
+
+	$('.clickTab').hover(function() {
+		$( this ).addClass( "focus" );
+		$( '.clickTab' ).not( $( this ) ).removeClass( 'focus' );
+		
+		var self  = $( this ).index();
+		var	matchedTarget = $( '.clickTarget' ).eq(self);
+
+		matchedTarget.css("display", "block" );
+
+		if ($( this ).index() <= 2)
+			$( '#park, #beach, #zoo' ).not(matchedTarget).css("display", "none");
+		else 
+			$( '#mon, #tues, #wed' ).not(matchedTarget).css("display", "none");
+		
+
+	//need to refactor the HTML
+
+
+	});
+
 });
